66100f981c0824165d14e1dbfe296fa99a3a24cd
Taitil Chheda <taitilchheda@gmail.com>
2025-03-22 13:29:51 +0530

    updated frontend code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import Main from "./pages/App"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Recommendation from "./pages/Recommendation"
+import HomePage from "./pages/Home";
+import EventDetailPage from "./pages/EventDetails";
 function App() {
 
 
@@ -10,6 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Main/>}/>
           <Route path="/recommend" element={<Recommendation />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/event/:id" element={<EventDetailPage />} />
+          <Route path="/movie/:id" element={<EventDetailPage />} />
+          {/* </Route> */}
         {/* </Route> */}
       </Routes>
     </BrowserRouter>
